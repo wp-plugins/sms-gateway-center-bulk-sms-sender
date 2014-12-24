344e776898ab152be3fb174111036ebcbe845bb7
smsgatewaycenter <smsgatewaycenter@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-12-24 05:27:25 +0000

    version 1.2
    git-svn-id: https://plugins.svn.wordpress.org/sms-gateway-center-bulk-sms-sender/trunk@1053169 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- sgcsms.php
+++ sgcsms.php
@@ -3,7 +3,7 @@
 Plugin Name: SMS Gateway Center Bulk SMS Sender
 Plugin URI: http://www.smsgatewaycenter.com
 Description: Wp plugin to send bulk SMS from wp sites. Send Bulk SMS from any wp site. This is a plugin for www.smsgatewaycenter.com. You will have to be a registered member to use this plugin.
-Version: 1.1
+Version: 1.2
 Author: Raghhav Sammrat
 Author URI: http://www.smsgatewaycenter.com
 License: GPL3
@@ -38,6 +38,8 @@ Text Domain: SGCSMS
 		add_submenu_page('sgcsms', 'Send Bulk SMS', 'Send Bulk SMS', '1', 'sgcsms_send','bulksmssender');
 		add_submenu_page('sgcsms', 'Delivery Report', 'Delivery Report', '1', 'sgcsms_dlr','sgcdeliveryreport');
 		add_submenu_page('sgcsms', 'Short Code Report', 'Short Code Report', '1', 'sgcsms_shortcode','sgcshortcodereport');
+		add_submenu_page('sgcsms', 'Check Balance', 'Check Balance', '1', 'sgcsms_checkbalance','sgc_checkbalance');
+		add_submenu_page('sgcsms', 'Change Password', 'Change Password', '1', 'sgcsms_changepass','sgc_change_password');
 		add_submenu_page('sgcsms', 'SMS Account Settings', 'Settings', '1', 'sgcsms_settings','sgcsettings');
 	}
 	
@@ -96,8 +98,8 @@ Text Domain: SGCSMS
 					curl_setopt($ch, CURLOPT_POSTFIELDS, $param);
 					$curl_scraped_page = curl_exec($ch); 
 					curl_close($ch);
-				echo $curl_scraped_page."<br><br>";
-				echo "<p><a href=\"javascript:history.back(-1)\">Send New Message</a></p>";
+					echo $curl_scraped_page."<br><br>";
+					echo "<p><a href=\"javascript:history.back(-1)\">Send New Message</a></p>";
 				break;
 			default:
 				echo "<img style=\"margin:30px;clear:both\" src='".plugins_url( 'admin-logo.png', __FILE__ )."' alt=\"SMS Gateway Center\" border=\"0\" />";
@@ -108,6 +110,66 @@ Text Domain: SGCSMS
 
 	}
 
+	function sgc_change_password() {
+		global $sgcoption, $username, $password, $settingsfile;
+		$newpass = $_REQUEST["newpass"];
+		$confirmpass = $_REQUEST["confirmpass"];
+		switch ($sgcoption){
+			case sgcpasschange:
+				echo "<img style=\"margin:30px;clear:both\" src='".plugins_url( 'admin-logo.png', __FILE__ )."' alt=\"SMS Gateway Center\" border=\"0\" />";
+				echo "<h1>Change Password - SMS Gateway Center</h1>";
+				
+				if ($newpass == "") {
+					echo "<p>Error!<br>New Password cannot be blank<br><a href=\"javascript:history.back(-1)\">Go Back</a></p>";
+					die;
+				} 
+				elseif  ($confirmpass == "") {
+					echo "<p>Error!<br>Confirm Password cannot be blank<br><a href=\"javascript:history.back(-1)\">Go Back</a></p>";
+					die;
+				} 
+				else {
+					if($newpass == $confirmpass)
+					{
+						$url = "http://www.smsgatewaycenter.com/library/wp/change_password.php";
+
+						$param['Username'] = $username;
+						$param['OldPassword'] = urlencode($password);
+						$param['NewPassword'] = urlencode($newpass);
+
+						if (!$ch = curl_init()) {
+							echo "Could not initialize cURL session.";
+							exit;
+						}
+						$ch = curl_init($url);
+						curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+						curl_setopt($ch, CURLOPT_POST, true);
+						curl_setopt($ch, CURLOPT_POSTFIELDS, $param);
+						$curl_scraped_page = curl_exec($ch); 
+						curl_close($ch);
+						list ($status, $cpmessage) = split('\|', $curl_scraped_page);
+						if ($status == "SUCCESS " || $status == "SUCCESS" ) {
+							$f=fopen("$settingsfile","w");
+							fwrite($f,"<?php\n\$username=\"$username\";\n\$password=\"$newpass\";\n?>");
+							fclose($f);
+						}
+						echo $curl_scraped_page."<br><br>";
+						echo "<p><a href=\"javascript:history.back(-1)\">Go back</a></p>";
+					}
+					else {
+						echo "<p>Error!<br>New Password and Confirm Password do not match<br><a href=\"javascript:history.back(-1)\">Go Back</a></p>";
+						die;
+					}
+				}
+
+				break;
+			default:
+				echo "<img style=\"margin:30px;clear:both\" src='".plugins_url( 'admin-logo.png', __FILE__ )."' alt=\"SMS Gateway Center\" border=\"0\" />";
+				echo "<h1>Bulk SMS Sender - SMS Gateway Center</h1>"."<form method=post action=\"";
+				echo str_replace( '%7E', '~', $_SERVER['REQUEST_URI']);
+				echo "&sgcoption=sgcpasschange\">"."<p><b>New Password:</b><br><input type=\"password\" name=\"newpass\"></p>"."<p>New Password:</b><br><input type=\"password\" name=\"confirmpass\"></p>"."<p><input type=submit class=button name=submit value=\"Change Password\"></p>"."</form>";
+		}
+
+	}
 	
 	function sgcsettings(){
 		global $sgcoption, $settingsfile, $username, $password;
@@ -181,5 +243,39 @@ Text Domain: SGCSMS
 		$curl_scraped_page = curl_exec($ch); 
 		curl_close($ch);
 		echo $curl_scraped_page."<br><br>";
+	}
+	
+	function sgc_checkbalance(){
+		global $sgcoption, $sgcsmsfolder, $username, $password;
+		echo "<img style=\"margin:30px;clear:both\" src='".plugins_url( 'admin-logo.png', __FILE__ )."' alt=\"SMS Gateway Center\" border=\"0\" />";
+		echo "<h1>Check SMS Balance - SMS Gateway Center</h1>";
+		$url = "http://www.smsgatewaycenter.com/library/checkbalance.php";
+
+		$param['Username'] = $username;
+		$param['Password'] = urlencode($password);
+				
+		if (!$ch = curl_init()){
+			echo "Could not initialize cURL session.";
+			exit;
+		}
+
+		$ch = curl_init($url);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+		curl_setopt($ch, CURLOPT_POST, true);
+		curl_setopt($ch, CURLOPT_POSTFIELDS, $param);
+		$curl_scraped_page = curl_exec($ch); 
+		curl_close($ch);
+		list ($balance, $expiry) = split('\|', $curl_scraped_page);
+		list($balancen, $seperator) = split('\ ', $balance);
+		if ($balancen == "Balance " || $balancen == "Balance" ) {
+			echo "<table class='wp-list-table widefat fixed posts'><thead><tr>
+					<th scope='col' id='mobile' class='manage-column column-title'><span>Balance</span></th>
+					<th scope='col' id='message' class='manage-column column-message'>Expiry</th></tr></thead><tbody id='the-list'>";
+					preg_match( "/Balance : (\d+)/", $balance, $balancematches);
+					$showbalance = $balancematches[1];
+			echo "<tr><td><b>".$showbalance."</b> SMS Credits</td><td>".str_replace('Expire Date : ', '', $expiry)."</td></tr></tbody></table>";
+		} else {
+			echo $curl_scraped_page."<br><br>";
+		}
 	}
 	?>
